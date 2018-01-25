33ce290729d7b8b4d84f67b4ee45102e819aaac1
Alina Leuca <alinaleuca@gmail.com>
2018-01-25 20:12:18 +0100

    add main route with pug template

--- app.js
+++ app.js
@@ -4,13 +4,22 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const port = 8080;
+const pug = require('pug');
 
 // Created a route for the bloggersteampage. Its working local on my computer.
 // This is a static page/route to that's why it's made in an html and not in a templating engine
 // Since we also have a dynamic page (the blogposts) we can use PUG there to practive with two different ways of RESpons in Node.js
 
-app.get("/bloggers", (req, res) => {
+app.set('view engine', 'pug');
 
+// main route that also renders a pug template
+app.get("/", function(req, res) {
+    res.render('index');
+})
+
+app.get("/bloggers", (req, res) => {
+    // renders html file with fs, eventually needs to be moved to a pug template as well
     fs.readFile("./bloggersteampage.html", "utf-8", (err, data) => {
         if (err) {
           res.status(500); // Internal Server Error
@@ -27,4 +36,4 @@ app.get("/bloggers", (req, res) => {
 
 
 // Listen to port 8080
-app.listen(8080, () => console.log("Server listening to port 8080!"));
+app.listen(port, () => console.log("Server listening to port 8080!"));
